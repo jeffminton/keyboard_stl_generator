bd6eee9c75af2e33ec0b075b15c429ed7579fbb5
Jeffrey Minton <jeffrey.minton@gmail.com>
2022-03-05 13:56:10 -0800

    Modify cable holder to use class variables instead of random hard coded values

--- cable.py
+++ cable.py
@@ -15,28 +15,33 @@ class Cable():
 
         self.parameters = parameters
 
+        # length and thickness of the pabck pannel that exends beyond the cable hole 
+        # to prevent the holder from falling into the case
         self.stop_plate_thickness = 1
-
         self.stop_plate_overhang = 3
 
+        # Clip parameters
+        # Clip triangle parameters
         self.clip_short_edge = 1.2
-
         self.clip_long_edge = 6
-
         self.clip_arm_thickness = 1
-
+        # spacing between the clip and the cable hole wall
         self.clip_body_gap = 0.75
 
+        # holder support block parameters
+        # The spacing between the clips and the suport block
         self.inner_block_clip_offset = 1
+        # Width ofthe support block
+        self.inner_block_width = self.parameters.cable_hole_width - (self.inner_block_clip_offset * 2) - (self.clip_arm_thickness * 2) 
 
-        self.kerf = self.parameters.kerf * 2
 
+        # Gap to allow between the cable hole size and the clip size
+        # Needed to accounts for the impoerfections in the printing that can cause holes to be smaller than defined
         self.cable_hole_holder_offset = 0.5
 
         self.cable_holder_height = self.parameters.cable_hole_height - self.cable_hole_holder_offset # (self.parameters.kerf * 4)
 
-        self.inner_block_width = self.parameters.cable_hole_width - (self.inner_block_clip_offset * 2) - (self.clip_arm_thickness * 2) 
-
+        # Parameters to shape the cable holder hole into an elipse that is either horizontal or vertical
         self.elipse_vertical = False
         self.elipse_horizontal = True
         self.elipse_ratio = .1
@@ -50,7 +55,15 @@ class Cable():
             self.x_scale = self.x_scale - self.elipse_ratio
             self.y_scale = self.y_scale + self.elipse_ratio
 
-        self.clamp_percentage = .9
+        # Clamp parameters
+        # The width of the clapm is a percentage of the width of the suport block
+        # This defines the clamp percentage
+        self.clamp_percentage = 1.0
+        self.clamp_holder_gap = .05 # self.parameters.kerf * 2
+        self.clamp_block_height = self.cable_holder_height / 4
+        self.clamp_block_width = (self.inner_block_width * self.clamp_percentage)# - (self.clamp_holder_gap * 2)
+        # Set a value that will reduce the height of the cable hole when the clamp is inserted
+        self.clamp_hole_offset = .1
 
 
         self.logger.debug('Cable Hole Witdh: %f', self.parameters.cable_hole_width)
@@ -61,15 +74,21 @@ class Cable():
 
     def holder_full(self):
 
+        # Calculate the total stop plate width
         cable_hole_plate_width = self.parameters.cable_hole_width + (self.stop_plate_overhang * 2)
         # cable_hole_plate_height = self.parameters.cable_hole_height# + (self.stop_plate_overhang * 2)
         
+        # Create stop plate. The stop clock only extends to the sides of the cable holder
         holder = cube([cable_hole_plate_width, self.cable_holder_height, self.stop_plate_thickness], center = True)
 
+        # Get clip object
         clip_part = self.clip()
+
+        # Move clip to the right and mirror it for the left side clip
         clip_part = right((self.parameters.cable_hole_width / 2) - self.clip_arm_thickness - (self.cable_hole_holder_offset / 2)) (clip_part)
         clip_part += mirror([1, 0, 0]) ( clip_part )
 
+        # Add clips to the holder
         holder += clip_part
 
         # Add block that fills the width of the wall for more support
@@ -77,7 +96,7 @@ class Cable():
             cube([self.inner_block_width, self.cable_holder_height, self.parameters.case_wall_thickness], center = True)
         )
 
-
+        # Remvoe the 
         holder -= self.holder_hole(include_slot = True)
 
         return holder
@@ -85,16 +104,21 @@ class Cable():
 
 
     def holder_hole(self, include_slot = False):
+        
+        # Main cable hole
         hole = cylinder(
             r = self.parameters.cable_diameter / 2, 
             h = self.parameters.case_wall_thickness * 4, 
             center = True
         )
 
-        
+        # Transform cable hole into elipse in direction defined above
         hole = scale([self.x_scale, self.y_scale, 1.0]) ( hole )
 
+
         if include_slot == True:
+            # Include a slot above the hole that will accept the cable and clamp
+            # Slot object
             slot = forward(self.parameters.cable_diameter * 2, ) ( 
                 cube(
                     [
@@ -106,6 +130,7 @@ class Cable():
                 )
             )
 
+            # Scale slot to match the x size of the hole elipse
             slot = scale([self.x_scale, 1, 1]) ( slot )
             
             hole += slot
@@ -115,6 +140,7 @@ class Cable():
 
     def clip(self):
 
+        # Clip polygon
         poly_points = [
             [0, 0],
             [0, self.parameters.case_wall_thickness + self.clip_long_edge],
@@ -136,36 +162,42 @@ class Cable():
 
     
     def holder_main(self):
-
+        
+        # Get full holder
         holder_main_body = self.holder_full()
 
-        clamp_remove_block_height = self.cable_holder_height / 4
+        # Create block that will be removed from the main holde rto make space for the clamp
+        clamp_remove_block = self.holder_clamp(clamp_remove_block = True)
 
-        clamp_remove_block = forward((self.cable_holder_height / 2) - (clamp_remove_block_height / 2)) ( 
-            cube([self.inner_block_width * self.clamp_percentage, clamp_remove_block_height, self.parameters.case_wall_thickness * 4], center = True) 
-        )
-
+        # Remove clamp remove block from main holder
         holder_main_body -= clamp_remove_block
 
         return holder_main_body
 
 
 
-    def holder_clamp(self):
+    def holder_clamp(self, clamp_remove_block = False):
+        # Declare parameters used to provide spacing for the clamp in the main cable holder
+        clamp_holder_gap = self.clamp_holder_gap
+        clamp_holder_extra = 0.0
 
-        clamp_block_height = self.cable_holder_height / 4
-        clamp_block_width = (self.inner_block_width * self.clamp_percentage) - (self.kerf * 2)
-
+        if clamp_remove_block == True:
+            clamp_holder_gap = 0.0
+            clamp_holder_extra = self.clamp_holder_gap
+        
+        # Set the depth of the clapm and include the stop plate tickness and space for the sorrinuding clamp blocks
         clamp_depth = self.parameters.case_wall_thickness + (self.stop_plate_thickness * 3)
 
-        clamp_block = forward((self.cable_holder_height / 2) - (clamp_block_height / 2)) ( 
-            cube([clamp_block_width - .1, clamp_block_height, clamp_depth], center = True) 
+        # Create clamp block
+        clamp_block = forward((self.cable_holder_height / 2) - (self.clamp_block_height / 2) + (clamp_holder_gap)) ( 
+            cube([self.clamp_block_width + (clamp_holder_extra * 2), self.clamp_block_height - (clamp_holder_gap * 2), clamp_depth], center = True) 
         )
 
-        slot = forward(self.cable_holder_height / 4) ( 
+        # Create strip that will fit into the cable hole slot
+        strip = forward(self.clamp_block_height) ( 
             cube(
                 [
-                    self.parameters.cable_diameter - .1, 
+                    self.parameters.cable_diameter - (clamp_holder_gap * 2), 
                     self.cable_holder_height / 2, 
                     clamp_depth
                 ], 
@@ -173,26 +205,33 @@ class Cable():
             )
         )
 
-        clamp_block += scale([self.x_scale, 1, 1]) ( slot )
+        # Scale strip to match x sacale of cable hole
+        clamp_block += scale([self.x_scale, 1, 1]) ( strip )
 
+        # Move block up to allign with holder body
         clamp_block = up((self.parameters.case_wall_thickness / 2)) ( clamp_block )
 
-        surround_block = forward(self.cable_holder_height / 4) ( 
+        # Set offset for surround block height
+        surround_block_height_offset = (clamp_holder_gap * 6)
+
+        # Create surround block
+        surround_block = forward((surround_block_height_offset / 2)) ( # forward(self.clamp_block_height - self.clamp_hole_offset) ( 
             cube(
                 [
-                    clamp_block_width - .1, 
-                    self.cable_holder_height / 2, 
-                    self.stop_plate_thickness - self.kerf * 2
+                    self.clamp_block_width + (clamp_holder_extra * 2), 
+                    (self.cable_holder_height) - surround_block_height_offset,# + self.clamp_hole_offset, 
+                    self.stop_plate_thickness - (clamp_holder_gap * 2)
                 ], 
                 center = True
             )
         )
 
-        clamp_block += up((clamp_depth / 2) + self.stop_plate_thickness + self.kerf) ( surround_block )
+        # Add a instance of the curround block to one end and another end of the clamp block
+        clamp_block += up( ( clamp_depth / 2 ) + self.stop_plate_thickness + clamp_holder_gap) ( surround_block )
+        clamp_block += down(self.stop_plate_thickness + clamp_holder_gap) ( surround_block )
 
-        clamp_block += down(self.stop_plate_thickness + self.kerf) ( surround_block )
-
-        clamp_block -= back(self.kerf ) ( self.holder_hole() )
+        # Remove a 180 degree rotated version of the cable hole with a slot from the clamp block
+        clamp_block -= back(self.clamp_hole_offset) ( rotate(180, [0, 0, 1]) ( self.holder_hole(include_slot = True) ) )
 
         return clamp_block
 
@@ -200,6 +239,7 @@ class Cable():
 
     def holder_all(self):
 
+        # Get a version of the cable holder with the main holder and clamp in an assembled view
         return self.holder_main() + up(0) ( self.holder_clamp() )
 
 
